0b3a434a78dce1542f71977319032d226d69abb9
Eduardo Wessel <wesseleduardo@gmail.com>
2025-06-17 21:45:50 -0300

    Tudo por um cabeçalho

---
+++ Eclipse/WorkSpace/n25-java1-01/src/br/com/senaisp/bauru/aula05/Exercisio01.java
@@ -0,0 +1,27 @@
+package br.com.senaisp.bauru.aula05;
+
+import java.util.Scanner;
+
+public class Exercisio01 {
+
+	public static void main(String[] args) {
+		//Usando o scanner para ler um arquivo texto
+		Scanner sc = new Scanner(Exercisio01.class.getResourceAsStream("arquivo.txt"));
+		String linha = sc.nextLine(); //pegando cabeçalho
+		Scanner sca = new Scanner(linha);
+		sca.useDelimiter(";");
+		//Imprimindo o cabeçalho
+		while(sca.hasNext()) {
+			System.out.print(sca.next() + "\t\t");  // \t = tab
+		}
+		System.out.println(); // pulando a linha
+		sca.close(); //fechando scanner de apoio
+		while(sc.hasNextLine()) {
+			linha = sc.nextLine();
+			sca = new Scanner(linha);
+			sca.useDelimiter(";");
+			
+		}
+	}
+
+}

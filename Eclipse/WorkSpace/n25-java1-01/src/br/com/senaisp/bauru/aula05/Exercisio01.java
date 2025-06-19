466d754164a8e10ef78a2b5c728f461d9669c0e9
Eduardo Wessel <wesseleduardo@gmail.com>
2025-06-18 21:49:21 -0300

    Cineminha

--- Eclipse/WorkSpace/n25-java1-01/src/br/com/senaisp/bauru/aula05/Exercisio01.java
+++ Eclipse/WorkSpace/n25-java1-01/src/br/com/senaisp/bauru/aula05/Exercisio01.java
@@ -1,5 +1,6 @@
 package br.com.senaisp.bauru.aula05;
 
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Exercisio01 {
@@ -18,9 +19,16 @@ public class Exercisio01 {
 		sca.close(); //fechando scanner de apoio
 		while(sc.hasNextLine()) {
 			linha = sc.nextLine();
-			sca = new Scanner(linha);
-			sca.useDelimiter(";");
-			
+			sca = new Scanner(linha).useLocale(Locale.US);
+			sca.useDelimiter(";");//Delimitador
+			//Imprimindo o nome
+			System.out.print(sca.next()+"\t"); //Pegando nome (Texto)
+			//Imprimindo a idade
+			System.out.print(sca.nextInt() + "\t");
+			//Imprimindo Salario
+			System.out.println(sca.nextDouble());//Salario
+			//fechando scanner
+			sca.close();
 		}
 	}
 
